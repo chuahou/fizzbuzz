9a6c0b740da4501e50aa7b3740f7c4fd47911146
Chua Hou <human+github@chuahou.dev>
2021-04-07 11:57:02 +0800

    wow

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,11 +1,16 @@
 /* SPDX-License-Identifier: MIT
  * Copyright (c) 2021 Chua Hou */
 
-i,t,f,j;
-g(){for(j=j^j;j++<2;)putchar(122);}
-main(){char s[3]={37,0x64,21^21};while(i++<25<<2){
-	if((t=i%3)*(f=i%5))printf(s,i);
-	if(!t){putchar('\a'*10);putchar(0x69);g();}
-	if(!f){putchar(0x42);putchar('2'*2+'"'/2);g();}
-	j=i<<5;while(--j!='('>>2);putchar(j);
-}}
+            i,t,f,j;
+           p(char c){
+          putchar(c);}
+         g(){for(j=j^j;
+        j++<2;0)p(122);}
+       main(){char s[3]={
+      37,100,21^21};while(
+     i++<25<<2){if((t=i%3)*
+    (f=i%5))printf(s,i);if(!
+   t){p('\a'*10);p(0x69);g();
+  }if(!f){p(0x42);p('2'*2+'"'/
+ 2);g();;}j=i<<5;while(--j!='('
+>>2);p(j);}puts("Am I hired?");}
