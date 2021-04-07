7c3b0430e6f77410b3992ab2ca9924c67d03c0da
Chua Hou <human+github@chuahou.dev>
2021-04-07 11:38:41 +0800

    fizzbuzz: init working program

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,4 +1,11 @@
 /* SPDX-License-Identifier: MIT
  * Copyright (c) 2021 Chua Hou */
 
-main() { printf("Hello\n"); }
+i,t,f,j;
+g(){for(j=j^j;j++<2;)putchar(122);}
+main(){char s[3]={37,0x64,21^21};while(i++<25<<2){
+	if((t=i%3)*(f=i%5))printf(s,i);
+	if(!t){putchar('\a'*10);putchar(0x69);g();}
+	if(!f){putchar(0x42);putchar('2'*2+'"'/2);g();}
+	j=i<<5;while(--j!='('>>2);putchar(j);
+}}
